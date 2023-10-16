95aa39687dc670e055b7a9521d3a929fc91c763d
Kevin Yu <yuzihe513@gmail.com>
2023-10-16 11:06:37 -0400

    Finished Order Feature part

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./Home/Home";
 import Products from "./Products/Products";
 import { Form } from 'react-router-dom';
+import Orders from "./Orders/Orders";
+import OrderDetail from './Orders/OrderDetail';
 
 function App() {
   const openMenu = () => {
@@ -30,6 +32,7 @@ function App() {
             <div className="header-links">
                 <Link to="/">Home</Link>
                 <Link to="/Catalog">Catalog</Link>
+                <Link to="/orders"><Orders /></Link>
             </div>
         </header>
         <aside className="sidebar">
@@ -46,8 +49,10 @@ function App() {
         </aside>
         <main className="main">
             <Routes>
-                <Route path="/" element={<Home/>} />
+                <Route path="/" element={<Home />} />
                 <Route path="/catalog" element={<Products />} />
+                <Route path="orders" element={<Orders />}></Route>
+                <Route path="/order/:id" element={<OrderDetail />}></Route>
             </Routes>
         </main>
         <footer className="footer">
